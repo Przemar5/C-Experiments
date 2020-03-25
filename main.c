efdbab399486b0d1c00394ac5fa0ac9dc57dfed0
Przemek Krogulski <przemar5@o2.pl>
2020-03-25 22:59:42 +0100

    first commit

--- main.c
+++ main.c
@@ -16,10 +16,30 @@ int mul2(int a)
     return a * 2;
 }
 
+int count(int a)
+{
+    return (int) (sizeof(a) / sizeof(a[0]));
+}
+
+int compose(int a, int (*arr)(int))
+{
+    int len = count(arr);
+    int a = arr[];
+
+    for (int i = 0; i < len; i++)
+    {
+        a = arr[i](a);
+    }
+
+    return a;
+}
+
 int main()
 {
     int (*arr[2])(int) = {add1, mul2};
     int a = arr[0](fun1(3));
+
+    a = compose(3, arr);
 
     printf("%d\n", a);
 
